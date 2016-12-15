94ba31e92ab12cfc872233e1799481cb37b4ea19
glstream <grayson.stream@gmail.com>
2016-12-15 12:42:16 -0800

    changes

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-var name = 'Grayson'
+var express = require('express');
+var app = express();
+var path = require('path');
+var routes = require('./api/routes');
 
 
-console.log("Hello "+ name);
+app.set('port', 3000);
+
+app.use('/css',function(req, res, next){
+    console.log("Page " + req.method+" "+ req.url+" was accesed" );
+    next();
+})
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', routes);
+
+var server = app.listen(app.get('port'), function(){
+    var port = server.address().port;
+    console.log("Magic port number " + port);
+});
+
+
