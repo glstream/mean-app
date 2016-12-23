c8719f95ced3dfa460c1026c36239e3b4dd9f2cb
glstream <grayson.stream@gmail.com>
2016-12-23 15:22:45 -0800

    git commit -m "geo Json"

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,46 +1,52 @@
 var mongoose = require('mongoose');
 var Hotel = mongoose.model('Hotel');
 
-var runGeoQuery = function (req, res){
-      var lng = ParseInt(req.query.lng);
-      var lat = ParseInt(req.query.lat);
 
-      //geo json types
-      var point = {
-        type : "Point",
-        coordinates : [lng, lat]
-      };
+var runGeoQuery = function(req, res) {
 
-      var geoOptions = {
-        spherical : true,
-        maxDistance : 2000,
-        num : 5 
-      };
+  var lng = parseFloat(req.query.lng);
+  var lat = parseFloat(req.query.lat);
 
-      Hotel
-        .geoNear(point, geoOptions, function (err, results, stats){
-          console.log("Geo results", results);
-          console.log("Geo stats", stats);
-          res
-            .status(200)
-            .json(results)
-        });
+  // A geoJSON point
+  var point = {
+    type : "Point",
+    coordinates : [lng, lat]
+  };
+
+  var geoOptions = {
+    spherical : true,
+    maxDistance : 2000,
+    num : 5
+  };
+
+  Hotel
+    .geoNear(point, geoOptions, function(err, results, stats) {
+      console.log('Geo Results', results);
+      console.log('Geo stats', stats);
+      res
+        .status(200)
+        .json(results);
+    });
+
 };
 
 module.exports.hotelsGetAll = function(req, res) {
 
+  console.log('GET the hotels');
+  console.log(req.query);
+
   var offset = 0;
   var count = 5;
 
-if (req.query && req.query.at && req.query.lng){
-  runGeoQuery( req, res );
-  return;
-}
+  if (req.query && req.query.lat && req.query.lng) {
+    runGeoQuery(req, res);
+    return;
+  }
 
   if (req.query && req.query.offset) {
     offset = parseInt(req.query.offset, 10);
   }
-  
+
   if (req.query && req.query.count) {
     count = parseInt(req.query.count, 10);
   }
@@ -49,64 +55,55 @@ if (req.query && req.query.at && req.query.lng){
     .find()
     .skip(offset)
     .limit(count)
-    .exec(function(err, hotels){
-      console.log("Found Hotels", hotels.length);
-      res 
+    .exec(function(err, hotels) {
+      console.log("Found hotels", hotels.length);
+      res
         .json(hotels);
     });
-
-
-  // collection
-  //   .find()
-  //   .skip(offset)
-  //   .limit(count)
-  //   .toArray(function(err, docs) {
-  //     console.log("Found hotels", docs.length);
-  //     res
-  //       .status(200)
-  //       .json(docs);
-  // });
 
 };
 
 module.exports.hotelsGetOne = function(req, res) {
-
-  var hotelId = req.params.hotelId;
-  
-  console.log('GET hotelId', hotelId);
+  var id = req.params.hotelId;
+  console.log('GET hotelId', id);
 
   Hotel
-    .findById(hotelId)
+    .findById(id)
     .exec(function(err, doc) {
       res
         .status(200)
         .json(doc);
-  });
+    });
 
 };
 
 module.exports.hotelsAddOne = function(req, res) {
+  console.log("POST new hotel");
   var db = dbconn.get();
   var collection = db.collection('hotels');
   var newHotel;
 
-  console.log("POST new hotel");
   if (req.body && req.body.name && req.body.stars) {
     newHotel = req.body;
     newHotel.stars = parseInt(req.body.stars, 10);
-    collection.insertOne(newHotel, function(err, response){
-          console.log(response.ops);
+    collection.insertOne(newHotel, function(err, response) {
+      console.log("Hotel added", response);
+      console.log("Hotel added", response.ops);
       res
         .status(201)
-        .json(response.op);
-    })
-  
-   
+        .json(response.ops);
+    });
+    // console.log(newHotel);
+    // res
+    //   .status(200)
+    //   .json(newHotel);
   } else {
     console.log("Data missing from body");
     res
       .status(400)
-      .json({ message : "Required data missing from body" });
+      .json({
+        message: "Required data missing from body"
+      });
   }
 
 };
